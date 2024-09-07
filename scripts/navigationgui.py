f620b2aa524ce7d6b3c749f6a642a0bc2b984f12
dcapacho3 <dcapacho3@gmail.com>
2024-09-07 15:05:34 -0500

    Gui - launch enable

--- scripts/navigationgui.py
+++ scripts/navigationgui.py
@@ -18,31 +18,50 @@ from robot_navigator import BasicNavigator, NavigationResult
 from ament_index_python.packages import get_package_share_directory
 from guiwaypoint import AutonomousNavigator
 from std_msgs.msg import String
+import signal
+import subprocess
+import time
+import math
+import matplotlib.patches as patches
+
 
 class NavigationWindow(ctk.CTk):
     def __init__(self, master=None):
         super().__init__(master)
         self.master = master
+        
+         # Configuración de la interfaz
+         
+       
         self.title("Navigation Interface")
         self.geometry("%dx%d+0+0" % (self.winfo_screenwidth(), self.winfo_screenheight()))
         self.resizable(width=1, height=1)
 
         # Inicializar la lista de productos seleccionados
         self.selected_products = []
-
-        # Inicializar el nodo y el navegador de ROS
-        rclpy.init(args=None)
-        self.node = rclpy.create_node('navigate_node')
-        self.navigator = BasicNavigator()
-        self.odom_subscriber = self.node.create_subscription(Odometry, 'odom', self.odom_callback, 100)
-        self.current_pose = None
-        self.continue_nav_publisher = self.node.create_publisher(String, '/continue_nav', 10)
         
-        # Crear un hilo para el ciclo de eventos de ROS
-        ros_spin_thread = threading.Thread(target=self.spin_ros_node)
-        ros_spin_thread.daemon = True  # Daemon para que termine cuando la GUI se cierre
-        ros_spin_thread.start()
-
+        self.node = None
+        self.executor = None
+        self.navigator = None
+        self.odom_subscriber = None
+        self.continue_nav_publisher = None
+        self.current_pose = None
+        
+        self.launch_processes = []
+        self.launch_thread = None
+        
+        self.ros_thread = threading.Thread(target=self.init_ros)
+        self.ros_thread.daemon = True  # Daemon para que termine cuando la GUI se cierre
+        self.ros_thread.start()
+        
+        
+        self.robot_patch = None
+        self.robot_width = 0.15  # in meters
+        self.robot_length = 0.35  # in meters
+        
+        
+        
+        
         # Variable para controlar el estado del botón
         self.navigation_started = False
         self.continue_nav_published = False  # Nueva variable para controlar la publicación
@@ -90,14 +109,25 @@ class NavigationWindow(ctk.CTk):
         self.button_inner_frame = ctk.CTkFrame(self.button_frame, height=100, width=100)
         self.button_inner_frame.pack(side=ctk.LEFT, fill=ctk.BOTH, expand=True, padx=10, pady=10)
 
+        
+        self.start_calibration_button = ctk.CTkButton(
+            self.button_inner_frame,
+            text="Iniciar Navegacion",
+            command=self.start_calibration,  # Nueva función que realizarás
+            height=80,
+        )
+        self.start_calibration_button.pack(side=ctk.LEFT, fill=ctk.BOTH, expand=True, padx=5)
+
+
         # Botón "Iniciar Navegación" con mayor altura
         self.start_navigation_button = ctk.CTkButton(
             self.button_inner_frame,
-            text="Iniciar Navegación",
+            text="Siguiente producto",
             command=self.start_navigation,
             height=80,
         )
         self.start_navigation_button.pack(side=ctk.LEFT, fill=ctk.BOTH, expand=True)
+        
 
         # Crear y mostrar el gráfico
         self.create_plot(self.map_frame)
@@ -110,14 +140,41 @@ class NavigationWindow(ctk.CTk):
         
         
         
+    def init_ros(self):
+       rclpy.init(args=None)
+       self.node = rclpy.create_node('navigate_node')
+       self.executor = rclpy.executors.SingleThreadedExecutor()
+       self.executor.add_node(self.node)
+       self.navigator = BasicNavigator()
+       self.odom_subscriber = self.node.create_subscription(Odometry, 'odom', self.odom_callback, 10)
+       self.continue_nav_publisher = self.node.create_publisher(String, '/continue_nav', 10)
+        
+    # Spin the executor in a loop
+       while rclpy.ok():
+          self.executor.spin_once(timeout_sec=0.1)
+
+
+        
       
     def odom_callback(self, msg):
         self.current_pose = {
             'x': msg.pose.pose.position.x,
-            'y': msg.pose.pose.position.y
+            'y': msg.pose.pose.position.y,
+            'orientation': self.get_yaw_from_quaternion(msg.pose.pose.orientation)
         }
-        print(f"Received odometry: x={self.current_pose['x']}, y={self.current_pose['y']}")
         
+           
+       
+       # print(f"Received odometry: x={self.current_pose['x']}, y={self.current_pose['y']}")
+      
+    def get_yaw_from_quaternion(self, quaternion):
+        # Convert quaternion to Euler angles
+        x, y, z, w = quaternion.x, quaternion.y, quaternion.z, quaternion.w
+        t3 = 2.0 * (w * z + x * y)
+        t4 = 1.0 - 2.0 * (y * y + z * z)
+        yaw = math.atan2(t3, t4)
+        return yaw
+      
     def spin_ros_node(self):
         rclpy.spin(self.node)
 
@@ -137,17 +194,29 @@ class NavigationWindow(ctk.CTk):
         if self.current_pose:
             # Convert map coordinates to pixel coordinates
             pixel_x = int((self.current_pose['x'] - self.origin[0]) / self.resolution)
-            pixel_y = int((-self.current_pose['y'] - self.origin[1]) / self.resolution)
+            pixel_y = int((self.current_pose['y'] - self.origin[1]) / self.resolution)
            # print(f"Robot position: pixel_x={pixel_x}, pixel_y={pixel_y}")  # Debug print
 
+            orientation = self.current_pose.get('orientation', 0)  # in radians
 
-        # Update the robot position on the plot
-            self.robot_position.set_data([pixel_x], [pixel_y])
-            self.canvas.draw()
+            # Update the robot's position and orientation
+            robot_width_pixels = self.robot_width / self.resolution
+            robot_length_pixels = self.robot_length / self.resolution
             
+            # Calculate the position for the rectangle's bottom-left corner
+            corner_x = pixel_x - robot_width_pixels / 2
+            corner_y = pixel_y - robot_length_pixels / 2
+
+            # Update the robot patch
+            self.robot_patch.set_xy((corner_x, corner_y))
+            self.robot_patch.set_angle(math.degrees(orientation))
+
+            self.canvas.draw()
         else:
-            print("No current pose available")  # Debug print
-        self.after(100, self.update_robot_position) 
+            print("No current pose available")
+        
+        self.after(100, self.update_robot_position)
+
 
     def actualizar_reloj_y_fecha(self):
         now = datetime.datetime.now()
@@ -162,7 +231,15 @@ class NavigationWindow(ctk.CTk):
 
         self.update_map_plot()
         
-        self.robot_position, = self.ax.plot([], [], 'bo', markersize=10, label='Robot')
+        #self.robot_position, = self.ax.plot([], [], 'bo', markersize=10, label='Robot')
+        
+        # Create the robot patch
+        robot_width_pixels = self.robot_width / self.resolution
+        robot_length_pixels = self.robot_length / self.resolution
+        self.robot_patch = patches.Rectangle((0, 0), robot_width_pixels, robot_length_pixels, 
+                                             fill=True, facecolor='blue', edgecolor='black')
+        self.ax.add_patch(self.robot_patch)
+
         self.ax.legend()
 
     def update_map_plot(self):
@@ -220,6 +297,9 @@ class NavigationWindow(ctk.CTk):
     def start_navigation(self):
         if not self.navigation_started:
             print("Iniciando navegación...")
+            print("Iniciando lanzamiento de archivos y navegación...")
+            
+            # Launch the ROS 2 launch files
             # Crear y ejecutar un hilo para la navegación
             navigation_thread = threading.Thread(target=self.run_navigation)
             navigation_thread.start()
@@ -227,6 +307,27 @@ class NavigationWindow(ctk.CTk):
         else:
             print("La navegación ya ha comenzado. Ejecutando otra acción...")
             self.perform_alternate_action()
+            
+            
+    def launch_ros2_files(self):
+        launch_commands = [
+            "ros2 launch my_agv_super mux.launch.py",
+            "ros2 launch my_agv_super navagv.launch.py"
+        ]
+        
+        devnull = open(os.devnull, 'w')
+        for cmd in launch_commands:
+            process = subprocess.Popen(
+                cmd, 
+                shell=True, 
+                stdout=devnull, 
+                stderr=subprocess.STDOUT
+            )
+            self.launch_processes.append(process)
+        
+        print("Launch files started successfully (output suppressed).")
+        
+        
 
     def run_navigation(self):
         # Esta función será ejecutada en un hilo separado
@@ -235,10 +336,20 @@ class NavigationWindow(ctk.CTk):
         print("Navegación completa")
         # Publicar el mensaje al finalizar la navegación
         self.publish_continue_nav()
+        
+        
+    def __del__(self):
+        # Terminate launch file processes when the window is closed
+        for process in self.launch_processes:
+            process.terminate()
+        
+        if self.node:
+            self.node.destroy_node()
+        rclpy.shutdown()
 
     def perform_alternate_action(self):
             self.publish_continue_nav()
-            self.continue_nav_published = True  # Marcar que el mensaje ha sido publicado
+            self.continue_nav_published = True  # Marcastartr que el mensaje ha sido publicado
             self.publish_stop()
 
     def publish_continue_nav(self):
@@ -250,7 +361,18 @@ class NavigationWindow(ctk.CTk):
         msg = String()
         msg.data = "stop"
         self.continue_nav_publisher.publish(msg)
-    
+        
+        
+# Nueva función que manejará la acción del nuevo botón
+    def start_calibration(self):
+        if not self.launch_thread or not self.launch_thread.is_alive():
+            self.launch_thread = threading.Thread(target=self.launch_ros2_files)
+            self.launch_thread.start()
+            self.start_calibration_button.configure(state="disabled")
+            print("Launching ROS 2 files in background...")
+        else:
+            print("ROS 2 launch files are already running.")
+
 
     def view_selected_products(self):
         self.selected_frame.destroy()  # Destruir el marco anterior
@@ -268,6 +390,8 @@ class NavigationWindow(ctk.CTk):
             product_name = row[0]
             label = ctk.CTkLabel(self.selected_frame, text=product_name)
             label.pack(pady=5)
+            
+         
 
 if __name__ == "__main__":
     app = NavigationWindow()
